f9ef75809eed4d7d5d8c94bdbd532a51752164b4
CHEN SHENG WEN <shengwenchen2006@gmail.com>
2023-03-04 23:34:04 +0800

    Leetcode75

---
+++ Leetcode75/Day 5 Greedy/Q121_121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_=0
+        min_=0
+        L=0
+        for i in range(1,len(prices)):
+            if prices[i]< prices[min_]:
+                min_ = i
+                max_ = i
+            if prices[i]>prices[max_]:
+                max_ = i
+            if prices[max_]-prices[min_]>L:
+                L = prices[max_]-prices[min_]
+        return L
